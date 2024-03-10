297fdcf806fdc9da102cbbc5d8bf532a36694308
chenyuchun@gmail.com <chenyuchun@gmail.com>
2024-03-10 23:02:06 +0800

    initialization Spring Boot project

---
+++ SampleMall/src/main/java/com/chun/samplemall/SampleMallApplication.java
@@ -0,0 +1,13 @@
+package com.chun.samplemall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleMallApplication.class, args);
+	}
+
+}
---
+++ SampleMall/src/test/java/com/chun/samplemall/SampleMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chun.samplemall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
